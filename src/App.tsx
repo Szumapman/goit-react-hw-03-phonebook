e865ea737e0834054680028f566be3fb5601e866
Paweł Szumański <pawel.szumanski@outlook.com>
2025-01-25 21:35:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import { ContactForm } from './components/ContactForm'
+import { Contact } from './types/Contact'
+import { INITIAL_STATE_TEST_DATA } from './constants/InitialStateTestData';
+import { State } from './interfaces/State';
+import { ContactList } from './components/ContactList';
+import { Filter } from './components/Filter';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState<State>(INITIAL_STATE_TEST_DATA);
+  const filteredContacts = state.contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(state.filter.toLowerCase());
+  });
+
+  const handleAddContact = (newContact: Contact) => {
+    if (state.contacts.some((contact) => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
+      alert(`${newContact.name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')} is already in contacts`);
+      return;
+    }
+   
+    setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+      filter: '',
+      name: '',
+      number: '',
+    }));
+  };
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    setState((prevState) => ({
+      ...prevState,
+      filter: event.target.value,
+    }));
+  };
+
+  const handleDeleteContact = (id: string) => {
+    setState((prevState) => ({
+      ...prevState,
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter filter={state.filter} onFilterChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </>
   )
 }
