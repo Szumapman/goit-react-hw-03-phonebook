ac7549dd38663827db265e5364bd59ba0f0f7055
Paweł Szumański <pawel.szumanski@outlook.com>
2025-01-30 17:58:35 +0100

    change the initial useState to use contacts from localStorage and add saving contacts to localStorage after every change

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { ContactForm } from './components/ContactForm'
 import { Contact } from './types/Contact'
-import { INITIAL_STATE_TEST_DATA } from './constants/InitialStateTestData';
+import { INITIAL_STATE_DATA } from './constants/InitialStateData';
 import { State } from './interfaces/State';
 import { ContactList } from './components/ContactList';
 import { Filter } from './components/Filter';
 
 
 function App() {
-  const [state, setState] = useState<State>(INITIAL_STATE_TEST_DATA);
+  const [state, setState] = useState<State>(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts ? { contacts:JSON.parse(storedContacts), filter: '' } : INITIAL_STATE_DATA;
+  });
+
   const filteredContacts = state.contacts.filter((contact) => {
     return contact.name.toLowerCase().includes(state.filter.toLowerCase());
   });
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(state.contacts));
+  }, [state.contacts]);
 
   const handleAddContact = (newContact: Contact) => {
     setState((prevState) => ({
